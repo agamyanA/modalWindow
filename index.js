447d27a2b44eee29ed9c414e7a5e5b7dd6b6076d
Alex <agamyan@outlook.com>
2021-03-04 14:35:10 +0400

    added product cards

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
- const  myModal = $.modal()
+const myModal = $.modal({
+    title: 'Modal',
+    closable: true,
+    content: `
+        <h4>Modal is working</h4>
+            <p>Lorem ipsum dolor sit.</p>`,
+    width: '400px',
+    footerButtons: [
+        {
+            text: 'OK', type: 'primary', handler() {
+                console.log('click primary')
+                myModal.close()
+            }
+        },
+
+        {
+            text: 'Cancel', type: 'danger', handler() {
+                console.log('click danger')
+                myModal.close()
+            }
+        }
+    ]
+})
+
+const myCard = $.card([
+    [
+        { id: 1, title: 'Яблоки', price: 20, img: 'https://e1.edimdoma.ru/data/ingredients/0000/2374/2374-ed4_wide.jpg?1487746348' },
+        { id: 2, title: 'Апельсины', price: 30, img: 'https://fashion-stil.ru/wp-content/uploads/2019/04/apelsin-ispaniya-kg-92383155888981_small6.jpg' },
+        { id: 3, title: 'Манго', price: 40, img: 'https://itsfresh.ru/upload/iblock/178/178d8253202ef1c7af13bdbd67ce65cd.jpg' },
+    ],
+    [
+        { id: 1, title: 'Яблоки', price: 20, img: 'https://e1.edimdoma.ru/data/ingredients/0000/2374/2374-ed4_wide.jpg?1487746348' },
+        { id: 2, title: 'Апельсины', price: 30, img: 'https://fashion-stil.ru/wp-content/uploads/2019/04/apelsin-ispaniya-kg-92383155888981_small6.jpg' },
+        { id: 3, title: 'Манго', price: 40, img: 'https://itsfresh.ru/upload/iblock/178/178d8253202ef1c7af13bdbd67ce65cd.jpg' },
+    ],
+    [
+        { id: 1, title: 'Яблоки', price: 20, img: 'https://e1.edimdoma.ru/data/ingredients/0000/2374/2374-ed4_wide.jpg?1487746348' },
+        { id: 2, title: 'Апельсины', price: 30, img: 'https://fashion-stil.ru/wp-content/uploads/2019/04/apelsin-ispaniya-kg-92383155888981_small6.jpg' },
+        { id: 3, title: 'Манго', price: 40, img: 'https://itsfresh.ru/upload/iblock/178/178d8253202ef1c7af13bdbd67ce65cd.jpg' },
+    ],
+    [
+        { id: 1, title: 'Яблоки', price: 20, img: 'https://e1.edimdoma.ru/data/ingredients/0000/2374/2374-ed4_wide.jpg?1487746348' },
+        { id: 2, title: 'Апельсины', price: 30, img: 'https://fashion-stil.ru/wp-content/uploads/2019/04/apelsin-ispaniya-kg-92383155888981_small6.jpg' },
+        { id: 3, title: 'Манго', price: 40, img: 'https://itsfresh.ru/upload/iblock/178/178d8253202ef1c7af13bdbd67ce65cd.jpg' },
+    ],
+    [
+        { id: 1, title: 'Яблоки', price: 20, img: 'https://e1.edimdoma.ru/data/ingredients/0000/2374/2374-ed4_wide.jpg?1487746348' },
+        { id: 2, title: 'Апельсины', price: 30, img: 'https://fashion-stil.ru/wp-content/uploads/2019/04/apelsin-ispaniya-kg-92383155888981_small6.jpg' },
+        { id: 3, title: 'Манго', price: 40, img: 'https://itsfresh.ru/upload/iblock/178/178d8253202ef1c7af13bdbd67ce65cd.jpg' },
+    ]
+])
--- plugins/modal.js
+++ plugins/modal.js
@@ -1,35 +1,99 @@
+function noop() {}
+
+function _createModalFooter(buttons = []) {
+    if (buttons.length === 0) {
+        return document.createElement('div')
+    }
+
+    const wrap = document.createElement('div')
+    wrap.classList.add('modal-footer')
+
+    buttons.forEach(btn => {
+        const button = document.createElement('button')
+        button.textContent = btn.text
+        button.classList.add('btn')
+        button.classList.add(`btn-${btn.type || 'secondary'}`)
+        button.onclick = btn.handler || noop
+
+        wrap.appendChild(button)
+    })
+
+    return wrap
+}
+
+function _creatCard(options) {
+    const DEFAULT_HEIGHT = '450px'
+    const productGrid = document.createElement('section')
+    productGrid.classList.add('productGrid')
+    document.body.appendChild(productGrid)
+
+    for (let i = 0; i < options.length; i++) {
+
+        const card = document.createElement('div')
+        card.classList.add('row')
+        document.body.appendChild(card)
+
+        for (let j = 0; j < options[i].length; j++) {
+            card.insertAdjacentHTML('afterbegin', `
+            <div class="col">
+                <div class="container">
+                    <div class="card" style="height: ${DEFAULT_HEIGHT}">
+                        <img src="${options[i][j].img}" class="card-img-top" style="height: 270px">
+                        <div class="card-body">
+                        <h5 class="card-title">${options[i][j].title}</h5>
+                        <p class="card-text">${options[i][j].price}</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>        
+            </div>
+    `)     
+        }  
+    
+        productGrid.appendChild(card)
+    }
+    
+    return productGrid
+}
+
 function _createModal(options) {
+    const DEFAULT_WIDTH = '600px'
     const modal = document.createElement('div')
     modal.classList.add('vmodal')
     modal.insertAdjacentHTML('afterbegin', `
-        <div class="modal-overlay">
-            <div class="modal-window">
+        <div class="modal-overlay" data-close="true">
+            <div class="modal-window" style="width: ${options.width || DEFAULT_WIDTH}">
                 <div class="modal-header">
-                    <span class="modal-title">Modal title</span>
-                    <span class="modal-close">&times;</span>
+                    <span class="modal-title">${options.title || 'window'}</span>
+                    ${options.closable ? `<span class="modal-close" data-close="true">&times;</span>` : ''}
                 </div>
-                <div class="modal-body">
-                    <p>Lorem ipsum dolor sit.</p>
-                    <p>Lorem ipsum dolor sit.</p>
-                </div>
-                <div class="modal-footer">
-                    <button>ok</button>
-                    <button>cancel</button>
+                <div class="modal-body" data-content>
+                    ${options.content || ''}
                 </div>
             </div>
         </div>
     `)
+    const footer = _createModalFooter(options.footerButtons)
+    modal.querySelector('[data-content]').after(footer)
     document.body.appendChild(modal)
     return modal
 }
 
-$.modal = function(options) {
+$.card = function (options) {
+    _creatCard(options) 
+}
+
+$.modal = function (options) {
     const ANIMATION_SPEED = 200
     const modal = _createModal(options)
     let closing = false
+    let destroyed = false
 
-    return {
+    const $modal = {
         open() {
+            if (destroyed) {
+                return console.log('Modal is destroyed');
+            }
             !closing && modal.classList.add('open')
         },
 
@@ -37,12 +101,28 @@ $.modal = function(options) {
             closing = true
             modal.classList.remove('open')
             modal.classList.add('hide')
-            setTimeout( () => {
+            setTimeout(() => {
                 modal.classList.remove('hide')
             }, ANIMATION_SPEED)
             closing = false
-        },
-
-        destroy() {}
+        }
     }
+
+    const listener = event => {
+        if (event.target.dataset.close) {
+            $modal.close()
+        }
+    }
+
+    modal.addEventListener('click', listener)
+
+    return Object.assign($modal, {
+        destroy() {
+            modal.remove()
+            destroyed = true
+        },
+        setContent(html) {
+            modal.querySelector('[data-content]').innerHTML = html
+        }
+    })
 }
